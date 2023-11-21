60b223fd63c45201a8419a8eeef94b9fffa59a91
morganfula <morgan.fula@me.com>
2023-11-21 18:51:43 +0100

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+	<div id="viewport">
+		<slot />
+
+		<ClientOnly>
+			<TheExperience />
+		</ClientOnly>
+	</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss"></style>
